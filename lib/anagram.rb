0b2d0e08a612e76a53fcd018e56d85beb028a5dc
Masen Matthews <masenmatthews@gmail.com>
2018-02-09 12:19:56 -0800

    establish first successful test for vowel check

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,10 +7,10 @@ class WordInput
   def vowel_check(input1, input2)
   @input1 = input1
   @input2 = input2
-  input_array = []
-  input_array.push(input1, input2)
-  if (input_array.include?('a', 'e', 'i', 'o', 'u', 'y') == false)
-    "please enter a valid word"
+  vowel_array = []
+  vowel_array.push(input1, input2)
+  if (vowel_array.include?('a') == false || vowel_array.include?('e') == false || vowel_array.include?('i') == false || vowel_array.include?('o') == false || vowel_array.include?('u') == false || vowel_array.include?('y') == false)
+  return "please enter a valid word"
   else
     "test"
   end
