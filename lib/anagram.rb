053b5df391324ba4ed5c3823076a88fbfd0f1b47
Masen Matthews <masenmatthews@gmail.com>
2018-02-09 15:30:02 -0800

    add anagram_check method, troubleshoot tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,3 @@
-puts 'Input first word'
-@input1 = gets.chomp
-puts 'Input second word'
-@input2 = gets.chomp
-
 class WordInput
   def initailize(input1, input2)
     @input1 = input1
@@ -14,19 +9,18 @@ class WordInput
     vowel_array.push(input1, input2)
     vowel_array.map(&:downcase)
       if (vowel_array.include?('a') == false || vowel_array.include?('e') == false || vowel_array.include?('i') == false || vowel_array.include?('o') == false || vowel_array.include?('u') == false || vowel_array.include?('y') == false)
-      return "please enter a valid word"
+      return "please enter valid words"
   end
 end
 
   def anagram_check(input1, input2)
-    anagram_array = []
-    anagram_array.push(input1, input2)
-    anagram_test_array = anagram_array.split("")
-    binding.pry()
-    main_test = anagram_test_array.sort
-    if @input1 == @input2
-    "These words are anagrams"
-  elsif @input1 != @input2
+    split_val1 = input1.split("")
+    split_val2 = input2.split("")
+    test_val1 = split_val1.sort
+    test_val2 = split_val2.sort
+    if test_val1 == test_val2
+     "These words are anagrams"
+  else
     return "These words are not anagrams"
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,14 +4,14 @@ require('anagram')
 
 describe('WordInput') do
   describe('#anagram') do
-    it("Tests for vowels and returns 'please enter a valid word' if there are none") do
-      input = WordInput.new()
-      expect(input.vowel_check("jfj", "jfj")).to(eq("please enter a valid word"))
-    end
+  it("Tests for vowels and returns 'please enter a valid word' if there are none") do
+    input = WordInput.new()
+    expect(input.vowel_check("jfj", "jfj")).to(eq("please enter a valid word"))
+  end
 
   it("Returns inputted values as lowercase") do
     input = WordInput.new()
-    expect(input.vowel_check("CAT", "DOG")).to(eq("cat, dog"))
+    expect(input.vowel_check("JFJ", "JFJ")).to(eq("jfj, jfj"))
   end
 
   it("Tests if two words are anagrams and returns corresponding result") do
